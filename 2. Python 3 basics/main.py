f3dce6c6833931272f9cd8a76ec989381fa25200
gruptbo2 <tunggultanvan@gmail.com>
2022-02-15 21:07:59 +0700

    update

--- 2. Python 3 basics/main.py
+++ 2. Python 3 basics/main.py
@@ -72,5 +72,30 @@ print(nums)
 # hello()
 #-----------------------------------------
 
-# Python program to illustrate
+# # Python program to illustrate
 
+# # Python program to illustrate
+# # function with main
+# def getInteger():
+#     result = int(input("Enter integer: "))
+#     return result
+
+# def Main():
+#     print("Started")
+
+#     # calling the getInteger function and
+#     # storing ints return value in the output variable
+#     output = getInteger()
+#     print(output)
+
+# # now we are required to tell Python
+# # for 'Main' function existence
+# if __name__=="__main__":
+#     Main()
+#---------------------------------------------------
+
+# Python program to illustrate
+# a simple for loop
+
+for step in range(5):
+    print(step)
