aaf57654ff9451f3c018ab12f1da81753dc1cd18
gruptbo2 <tunggultanvan@gmail.com>
2022-02-16 10:50:55 +0700

    update

--- 3. Python Keyword/main.py
+++ 3. Python Keyword/main.py
@@ -17,3 +17,41 @@
 # print(None == 0)
 # print(None == [])
 #-------------------------------------
+
+# showing logical operation
+# or (return True)
+print(True or False)
+
+#showing logical operation
+# and (returns False)
+print(False and True)
+
+# showing logical operation
+# not (returns false)
+print(not True)
+
+# showing logical operation
+# not (returns False)
+print(not True)
+
+# using "in" to check
+if 's' in 'geeksforgeeks':
+    print("s is part of geeksforgeeks")
+else:
+    print("s is not part of geeksforgeeks")
+
+# using "in" to loop through
+for i in 'geekforgeeks':
+    print(i, end=" ")
+
+print("\r")
+
+# using is to check object identity
+# string is immutable( cannot be changed once allocated)
+# hence occupy different memory location
+print(' ' is ' ')
+
+# using is to check object identity
+# string is immutable( can be changed once allocated)
+# hence occupy different memory location
+print({} is {})
